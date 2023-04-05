8fab65639d95cb2bf610dae538805d87c8b462c2
Maria Jenkins <macjenkin@gmail.com>
2023-04-05 06:47:05 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 const employee = {
     name: "Sherlock Holmes",
-    streetAddress: "221B Baker Street",
-};
+    address: "221B Baker Street",
+}
 
 function updateEmployeeWithKeyAndValue(employeeObject, key, value) {
-    return {
-        ...employeeObject,
-        [key]: value,
-    };
-};
+    return {...employeeObject, [key]: value}
+}
 
 function destructivelyUpdateEmployeeWithKeyAndValue(employeeObject, key, value) {
     employeeObject[key] = value;
-    return employeeObject;
-};
+    return employeeObject
+}
+
 function deleteFromEmployeeByKey(employeeObject, key) {
-    const newEmployee = {...employeeObject};
-    delete newEmployee[key];
-    return newEmployee;
-};
+    let copyOfEmployeeObject = {...employeeObject};
+    delete copyOfEmployeeObject[key];
+    return copyOfEmployeeObject
+}
 
 function destructivelyDeleteFromEmployeeByKey(employeeObject, key) {
     delete employeeObject[key];
-    return employeeObject;
-};
+    return employeeObject
+}
